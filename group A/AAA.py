9d44a26493f5b7006e4dc6c8b4acb0897c7fe9d9
lec0baby <dormih05@gmail.com>
2023-09-10 20:55:59 +0300

    nothing

--- group A/AAA.py
+++
@@ -1,3 +0,0 @@
-sh = int(input())
-man = int(input())
-print (man // sh, man % sh, sep = '\n')
---
+++ hw16.09/ex1.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+if a > b:
+    print('1-ое число больше 2-ого')
+elif a < b:
+    print('2-ое число больше 1-ого')
+else:
+    print('Числа равны')
