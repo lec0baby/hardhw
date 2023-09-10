ef33329ad834bf587ac655f716a28bd2d72eb287
lec0baby <dormih05@gmail.com>
2023-09-10 21:25:21 +0300

    1t

---
+++ hw16.09/ex2.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+f = int(input())
+if f - d == 0:
+    print('Делить на ноль нельзя!')
+else: 
+    print((a * b - c) / (f - d))
