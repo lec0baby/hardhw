bf76be1d014cab67db380b4ec71a15a2f836a4a6
lec0baby <dormih05@gmail.com>
2023-09-14 14:29:19 +0300

    last

---
+++ hw16.09/ex6.py
@@ -0,0 +1,6 @@
+n1 = float(input())
+n2 = n1 // 1
+if n1 - n2 == 0:
+    print('Целое')
+else:
+    print('Дробное')
---
+++ hw16.09/ex7.py
@@ -0,0 +1,7 @@
+x = int(input())
+y = int(input())
+sum = 0
+for i in range(x, y+1):
+    if i % 5 == 0:
+        sum = sum + i
+print(sum)
