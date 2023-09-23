715a788a9cbb703f729554e650a0aa70a12505e2
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-09-23 11:22:11 +0300

    mega reshenie

--- hw16.09/ex6.py
+++ hw16.09/ex6.py
@@ -1,5 +1,14 @@
-n1 = float(input())
-if n1 % 1 == 0:
+#n1 = float(input())
+#if n1 % 1 == 0:
+#    print('Целое')
+#else:
+#    print('Дробное')
+num = input()
+if '.' not in num:
+    num = num + '.0'
+spl = num.split('.')
+num1 = '.'.join(spl[1:])
+if int(num1) == 0:
     print('Целое')
 else:
     print('Дробное')
