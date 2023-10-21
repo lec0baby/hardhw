de09e5682ad6fa239214f152c42e1f4ddd174fe3
lec0baby <143869694+lec0baby@users.noreply.github.com>
2023-10-21 12:33:30 +0300

    Update ex4_3.py

--- dz4/ex4_3.py
+++ dz4/ex4_3.py
@@ -1,5 +1,10 @@
-def check(x):
-    return len(x) == len(set(x))
+def is_unique(x):
+    s = set()
+    for i in x:
+        if i in s: 
+            return False
+        s.add(i)
+    return True
 
 if __name__ == '__main__':
-    print(check(input()))
+    print(is_unique(input()))
