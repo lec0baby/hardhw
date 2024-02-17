18f72c0dde5a7f3707f5939b75731aed235e64aa
lec0baby <143869694+lec0baby@users.noreply.github.com>
2024-02-17 15:10:03 +0300

    hehe

--- matrix_pew_pew/mtr.py
+++ matrix_pew_pew/mtr.py
@@ -4,22 +4,16 @@ class Matrix:
         self.columns = columns
         self.elements = elements
 
-    
     def input_matrix(self): 
         self.elements = []
         self.rows, self.columns = int(input()), int(input())
         for i in range(self.rows):
             self.elements.append(list(map(int, input(f'Введите элементы строки №{i+1}: ').split())))        
-        for j in range(self.rows):
-            if len(self.elements[j]) != self.columns:
-                return 'Ошибка: неверное кол-во элементов в строке'
+            if len(self.elements[i]) != self.columns:
+                raise ValueError('Некорректно введены элементы строки')
         
     def __str__(self):
         output = ''
         for j in range(self.rows):
             output += ' '.join(str(i) for i in self.elements[j]) + '\n'
         return output + f'Количество строк: {self.rows}\n' + f'Количество столбцов: {self.columns}'
-
-m = Matrix()
-m.input_matrix()
-#print(m)
--- matrix_pew_pew/tests/test_matrix.py
+++ matrix_pew_pew/tests/test_matrix.py
@@ -1,12 +1,26 @@
 import pytest
-from matrix_pew_pew.mtr import Matrix
+from mtr import Matrix
 
-def test_init():
-    m = Matrix(2, 2, [[1, 1],[1, 1]])
-    assert m.rows == 2
-    assert m.columns == 2
-    assert m.elements == [[1, 1],[1, 1]]
+@pytest.mark.parametrize(
+        ('rows', 'columns', 'elements'),
+        [
+        (2, 2, [[1, 1],[1, 1]])
+        ]
+)
+def test_init(rows, columns, elements):
+    m = Matrix(rows, columns, elements)
+    assert m.rows == rows
+    assert m.columns == columns
+    assert m.elements == elements
     m1 = Matrix()
     assert m1.rows == 0
     assert m1.columns == 0
     assert m1.elements == []
+
+def test_input(mocker):
+    mocker.patch('builtins.input', side_effect = ['2', '3', '1 2 3', '4 5 6'])
+    matrix = Matrix()
+    matrix.input_matrix()
+    assert matrix.rows == 2
+    assert matrix.columns == 3
+    assert matrix.elements == [[1, 2, 3], [4, 5, 6]]
