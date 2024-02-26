0025d98b30165e5773894dba988696b125425446
lec0baby <dormih05@gmail.com>
2024-02-26 19:57:22 +0300

    arc

--- zavgor/arc/archieve.py
+++ zavgor/arc/archieve.py
@@ -1,12 +1,36 @@
-with open('in.txt', 'r') as f_in:
-    data = f_in.read()
+def archieve():    
+    with open("input.txt", "r") as f:
+        text = f.read()
 
-compressed_data = ''
-for char in data:
-    if char in ['.', ',', '!', '?', ':', ';']:
-        compressed_data += char + ''
-    else:
-        compressed_data += char
+    punctuation = {'.', ',', ';', ':', '!', '?'}
+    output = ''
+    skip_space = False
 
-with open('out.txt', 'w') as f_out:
-    f_out.write(compressed_data)
+    for char in text:
+        if skip_space:
+            if char != ' ':
+                output += char
+                skip_space = False
+        else:
+            if char in punctuation:
+                skip_space = True
+                output += char
+            elif char != ' ':
+                output += char
+
+    with open("output.txt", "w") as f:
+        f.write(output)
+        
+def dearchieve():
+    with open('output.txt', 'r') as file:
+        text = file.read()
+
+    text_with_spaces = ''
+    for char in text:
+        if char in ['.', ',', ':', ';', '!', '?']:
+         text_with_spaces += char + ' '
+        else:
+            text_with_spaces += char
+
+    with open('outputtt.txt', 'w') as file:
+        file.write(text_with_spaces)
